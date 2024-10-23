6c146d763bc61040bc32e9adf5fadc16af022601
ferbarcante <barcantef@gmail.com>
2024-10-23 10:41:11 -0300

    feat: criação do model setor

--- app/model/Pessoa.php
+++ app/model/Pessoa.php
@@ -1,16 +1,11 @@
 <?php
 
+namespace App\Model;
+
 class Pessoa{
     private $id;
     private $cpf;
     private $nome;
-
-    public function __construct($id, $cpf, $nome)
-    {
-        $this->id = $id;
-        $this->cpf = $cpf;
-        $this->nome = $nome;
-    }
 
     public function getId()
     {
