1f2ff56ba77f5d10b9b01264c5422525936ba29c
ferbarcante <barcantef@gmail.com>
2024-10-23 09:41:41 -0300

    feat: criação do model fornecedor

---
+++ app/model/GrupoItem.php
@@ -0,0 +1,33 @@
+<?php
+
+class GrupoItem
+{
+    private $id;
+    private $codigo;
+    private $nome;
+    private $descricao;
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setCodigo($codigo){
+        $this->codigo = $codigo;
+    }
+
+    public function getCodigo(){
+        return $this->codigo;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setDescricao($descricao){
+        $this->descricao = $descricao;
+    }
+}
--- app/model/Pessoa.php
+++ app/model/Pessoa.php
@@ -14,7 +14,20 @@ class Pessoa{
     public function getId(){
         return $this->id;
     }
+
+    public function getCpf(){
+        return $this->cpf;
+    }
+
     public function setCpf($cpf){
         $this->cpf = $cpf;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    public function getNome(){
+        return $this->nome;
     }
 }
