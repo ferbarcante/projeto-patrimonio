640b19967fc2c681dc697d104656ffab1f959f3e
Rodrigo Santos <rodrigosantossilva1557@gmail.com>
2024-10-24 14:12:02 -0300

    chore: envio da view de cadastro pessoa para pasta Pessoa

---
+++ App/Views/Pessoa/cadastro_pessoa.php
@@ -0,0 +1,27 @@
+<?php include '../views/navbar.php' ?>
+
+<title>Cadastro de pessoa</title>
+
+<div class="container">
+    <h3 class="mt-3">Cadastro de Pessoa</h3>
+    <form action="" method="">
+
+        <div class="mb-3">
+            <input type="text" class="form-control" id="" name="id_pessoa" readonly>
+        </div>
+
+        <div class="mb-3">
+            <label for="nm_pessoa" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="" name="nm_pessoa"> 
+        </div>
+
+        <div class="mb-3">
+            <label for="sg_cpf" class="form-label">CPF</label>
+            <input type="text" class="form-control" id="" name="sg_cpf"> 
+        </div>
+
+        <button type="submit" class="btn btn-primary">Salvar</button>
+        <button type="button" class="btn btn-secondary">Voltar</button>
+    </form>
+</div>
+
