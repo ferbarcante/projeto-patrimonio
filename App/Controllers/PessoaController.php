6ed679b2463a51444fdbd7c2adc67fe5ce36e1f3
ferbarcante <barcantef@gmail.com>
2024-10-24 16:59:14 -0300

    feat: criação do index

--- App/Controllers/PessoaController.php
+++ App/Controllers/PessoaController.php
@@ -1,16 +1,25 @@
-<?php 
+<?php
 
 namespace App\Controllers;
 
+use App\Model\Pessoa;
+
 class PessoaController
 {
-    public function index()
-    {
-        require_once "../App/Views/cadastro_pessoa.php";
 
+    private $pessoa;
+
+    public function __construct(Pessoa $pessoa)
+    {
+        $this->pessoa = $pessoa;
     }
 
+    public function index()
+    {
+        $pessoa = $this->pessoa->getAll();
+
+        require_once "../App/Views/Pessoa/listar_pessoa.php";
+    }
+
+    
 }
-
-
-?>
--- App/Route.php
+++ App/Route.php
@@ -1,78 +1,62 @@
 <?php 
 
-    namespace app;
+namespace App;
 
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-    class Route {
+class Route {
 
-        private $routes;
-        // ROTAS QUE NOSSA APLICAÇÃO POSSUI
+    private $routes;
 
-        public function __construct()
-        {
-            $this->initRoutes();
-            $this->run($this->getUrl());
-        }
+    public function __construct()
+    {
+        $this->initRoutes();
+    }
 
-        public function getRoutes()
-        {
-            return $this->routes;
-        }
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 
-        public function setRoutes($routes)
-        {
-            $this->routes = $routes;
-        }
+    public function setRoutes($routes)
+    {
+        $this->routes = $routes;
+    }
 
-        protected function initRoutes() 
-        {
-            $routes['home'] = array(
-                'route' => '/home',
-                'controller' => 'HomeController',
-                'action' => 'index'
-            );
+    protected function initRoutes() 
+    {
+        $routes['home'] = [
+            'route' => '/home',
+            'controller' => 'HomeController',
+            'action' => 'index'
+        ];
 
-            $routes['cadastroGrupoBem'] = array(
-                'route' => '/cadastro-grupo-bem',
-                'controller' => 'GrupoBemController',
-                'action' => 'cadastroGrupoBem'
-            );
+        $routes['pessoa'] = [
+            'route' => '/pessoa',
+            'controller' => 'PessoaController',
+            'action' => 'index'
+        ];
 
-            $routes['cadastroPessoa'] = array(
-                'route' => '/cadastro-pessoa',
-                'controller' => 'PessoaController',
-                'action' => 'index'
+        $this->setRoutes($routes);
+    }
 
-            );
-            
-
-            $this->setRoutes($routes);
-            
-        }
-
-        public function run($url)
-        {
-            foreach($this->getRoutes() as $key => $route)
-            {
-                if($url == $route['route'])
-                {
-                    $class = "App\\Controllers\\".ucfirst($route['controller']);
-
-                    $controller = new $class;
-
-                    $action = $route['action'];
-
-                    $controller->$action();
-                }
+    public function run($url)
+    {
+        foreach ($this->getRoutes() as $key => $route) {
+            if ($url == $route['route']) {
+                $class = "App\\Controllers\\" . ucfirst($route['controller']);
+                var_dump($route['controller']);
                 
+                $controller = new $class;
+                $action = $route['action'];
+                $controller->$action();
             }
         }
+    }
 
-        public function getUrl()
-        {
-            return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        }
-       
-    } 
+    public function getUrl()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+}
