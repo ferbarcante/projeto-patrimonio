2e71f5b1d1d4e75c7fefc006bedc0a826babd160
ferbarcante <barcantef@gmail.com>
2024-10-24 13:54:54 -0300

    feat crud pessoa

--- App/Models/Pessoa.php
+++ App/Models/Pessoa.php
@@ -41,28 +41,28 @@ class Pessoa{
     }
 
     public function create() {
-        $stmt = $this->db->prepare("INSERT INTO fornecedores (, ) VALUES (?, ?)");
+        $stmt = $this->db->prepare("INSERT INTO pessoa (, ) VALUES (?, ?)");
         return $stmt->execute([$this->nmPessoa, $this->cpfPessoa]);
     }
     
     public function getAll() {
-        $stmt = $this->db->query("SELECT * FROM fornecedores");
+        $stmt = $this->db->query("SELECT * FROM pessoa");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     
     public function getById($id) {
-        $stmt = $this->db->prepare("SELECT * FROM fornecedores WHERE id_fornecedor = ?");
+        $stmt = $this->db->prepare("SELECT * FROM pessoa WHERE idPessoa = ?");
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     
     public function update() {
-        $stmt = $this->db->prepare("UPDATE fornecedores SET nm_fornecedor = ?, sg_cnpjcpf = ? WHERE id_fornecedor = ?");
-        return $stmt->execute([$this->nmFornecedor, $this->sgCnpjcpf, $this->idFornecedor]);
+        $stmt = $this->db->prepare("UPDATE pessoa SET nmPessoa = ?, cpfPessoa = ? WHERE idPessoa = ?");
+        return $stmt->execute([$this->nmPessoa, $this->cpfPessoa, $this->idPessoa]);
     }
     
     public function delete($id) {
-        $stmt = $this->db->prepare("DELETE FROM fornecedores WHERE id_fornecedor = ?");
+        $stmt = $this->db->prepare("DELETE FROM pessoa WHERE idPessoa = ?");
         return $stmt->execute([$id]);
     }
     
