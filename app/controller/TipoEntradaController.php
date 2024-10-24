73a64cbd82157756d44c657f1c26ec240df2fc52
ferbarcante <barcantef@gmail.com>
2024-10-24 10:29:49 -0300

    feat: att tipo entrada controller

--- app/controller/TipoEntradaController.php
+++ app/controller/TipoEntradaController.php
@@ -5,18 +5,7 @@ use app\model\TipoEntrada;
 
 class TipoEntradaController 
 {
-    public function index()
-    {
-        
-    }
+   
 
-    public function create(TipoEntrada $tipoEntrada){
-        $sql = "INSERT INTO tipo_entrada (nome, descricao) VALUES (?)";
 
-        $stmt = Conexao::getConn()->prepare($sql);
-        $stmt->execute([$tipoEntrada->getNmTipoEntrada()]);
-
-    }
-
-    
 }
