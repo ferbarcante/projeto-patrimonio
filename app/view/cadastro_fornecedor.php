de9b2184f74c676e29f5cc34ed8ccd0c600310b5
Rodrigo Santos <rodrigosantossilva1557@gmail.com>
2024-10-24 08:36:38 -0300

    feat: criação de tela de cadastro de fornecedor

---
+++ app/view/cadastro_fornecedor.php
@@ -0,0 +1,25 @@
+<?php include '../view/navbar.php' ?>
+
+<title>Cadastro de Fornecedor</title>
+
+<div class="container">
+    <h3 class="mt-3">Cadastro de Fornecedor</h3>
+    <form action="" method="">
+        <div class="mb-3">
+            <input type="hidden" class="form-control" id="" name="id_fornecedor" readonly>
+        </div>
+
+        <div class="mb-3">
+            <label for="nm_fornecedor" class="form-label">Nome do Fornecedor</label>
+            <input type="text" class="form-control" id="" name="nm_fornecedor">
+        </div>
+
+        <div class="mb-3">
+            <label for="sg_cpf" class="form-label">Cpf</label>
+            <input type="text" class="form-control" id="" name="sg_cpf" maxlength="11">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Salvar</button>
+        <button type="button" class="btn btn-secondary mx-1" onclick="window.location='lista_fornecedor.php'">Voltar</button>
+    </form>
+</div>
